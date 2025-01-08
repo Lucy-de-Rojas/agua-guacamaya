57adca6536f3917c503a6034d95078bdd4143454
Lucy de Rojas <lucyderojas@gmail.com>
2025-01-08 17:51:07 +0000

    added home paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,14 +11,23 @@ export default function Home() {
 
 
   return (<Layout>
+<div className="text-white p-5 text-3xl">
 
 
-    <h1>Welcome to Agua Guacamaya</h1>
+    <p className="text-6xl mb-5  font-[robotoBOLD]">Welcome to Agua Guacamaya</p>
 
-    <p>We do lovely water</p>
+<hr className="my-3"/>
+
+
+    <p className="font-[robotoLIGHT]">Discover the pristine beauty of the Colombian Andes in every sip.
+
+Aguaguacamaya is a family-run business dedicated to providing you with the purest, most refreshing bottled water sourced directly from the heart of the Andes Mountains.
+
+Our water is naturally filtered through layers of volcanic rock, ensuring exceptional taste and quality. Ideal for shops and restaurants seeking a premium, sustainable beverage option.</p>
 
 
 
+</div>
 
 
 
